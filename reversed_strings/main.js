4a4a6699c2c6547ee05b60c1b4fbabad10b3317c
Jacqueline Regan <JackieReganDev@yahoo.com>
2021-02-23 22:58:39 -0500

    8kyu reversed strings

---
+++ reversed_strings/main.js
@@ -0,0 +1,7 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// function solution(str){
+//   return str.split('').reverse().join('')
+// }
+
+const solution = str => str.split('').reverse().join('')
